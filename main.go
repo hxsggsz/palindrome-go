f4fd374cdc5633950f7f490ebeea4fe77b099204
hxsggsz <vt.hugo.2021@gmail.com>
2023-12-16 16:01:34 -0300

    create script

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+	"strings"
+)
+
+func main() {
+	word := ""
+
+	fmt.Print("A word to check palindrome: ")
+	fmt.Scanln(&word)
+
+	isPalindrome := checkPalindrome(word)
+
+	if !isPalindrome {
+		fmt.Println("It's not a palindrome")
+		return
+	}
+
+	fmt.Println("It's a palindrome")
+}
+
+func checkPalindrome(word string) bool {
+	splitedWord := strings.Split(word, "")
+	slices.Reverse(splitedWord)
+	joinedReverseString := strings.Join(splitedWord, "")
+
+	return joinedReverseString == word
+}
